35e1156bd79fc663b2ab6002264128de1a972c43
Paramveer Singh <paramsinghkhattra@gmail.com>
2024-06-02 18:40:08 +0530

    Added home and auth routes

---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SignUp = () => {
+  return <div className="h2-bold"> SignUp</div>;
+};
+
+export default SignUp;
--- app/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Home() {
-  return <div className="h1-bold">Next JS 13, We are here!</div>;
-}
