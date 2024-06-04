58f43190c9018311b1fe1cc58b6a3da705997e40
Paramveer Singh <paramsinghkhattra@gmail.com>
2024-06-04 23:03:16 +0530

    Added Metadata and fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,17 +5,51 @@ import {
   SignedOut,
   UserButton,
 } from "@clerk/nextjs";
+
+import { Inter, Space_Grotesk } from "next/font/google";
+import type { Metadata } from "next";
+
 import "./globals.css";
 import React from "react";
+
+export const metadata: Metadata = {
+  title: "DevOverflow",
+  description:
+    "A community for developers to support each other and create an ecosystem that fosters global growth.",
+  icons: {
+    icon: "/assets/images/site-logo.svg",
+  },
+};
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
+});
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: "primary-gradient",
+          footerActionLink: "primary-text-gradient hover:text-primary-500",
+        },
+      }}
+    >
       <html lang="en">
-        <body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+          <h1 className="h1-bold">Dev Overflow</h1>
           <SignedOut>
             <SignInButton />
           </SignedOut>
